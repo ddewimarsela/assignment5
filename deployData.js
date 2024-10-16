9b2a1219df28c0e8411275ada45eb1816f2c75de
ddewimarsela <ddewimarsela@gmail.com>
2024-10-16 21:31:43 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Dewi Marsela Putri'; 
+
+const studentId = 'FS10563528'; 
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
